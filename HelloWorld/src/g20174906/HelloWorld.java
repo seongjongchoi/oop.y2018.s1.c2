11e3ff98fdc8cd48a3061f5d18201eef79f71637
최성종 <chois@uos.ac.kr>
2018-03-27 17:41:22 +0900

    20180327save

---
+++ HelloWorld/src/g20174906/HelloWorld.java
@@ -0,0 +1,17 @@
+package g20174906;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Your Name");
+		
+		int a = 10;
+		int b = 20;
+		int c;
+		c = a + b;
+		System.out.println(c);
+	}
+
+}
